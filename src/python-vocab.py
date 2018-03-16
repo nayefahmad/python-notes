89b8ed5faf0389096e62cadcd0c8a6b44de11063
nayefahmad <nayef.ahmad.11@gmail.com>
2018-03-16 13:46:31 -0700

    first

---
+++ src/python-vocab.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#*********************************************
+# PYTHON EXPERIMENTS 
+#*********************************************
+
+#**************************************
+# Basic arithmetic 
+#**************************************
+2**3 # exponent
+2//3 # floor divis? 
+2/3  # division
+
+x = 4 + 5 
+y=x-2
+y
+
+
+
+
+
+
+
+#**************************************
+# Strings, lists, dictionaries 
+#**************************************
+
+
+
+
+
+def addtwo (num): 
+    # takes a num, adds 2 if it's not 2 
+    if num != 2: 
+        return(num + 2)
+    else: 
+        return('nope')
+        
+addtwo(4)
+addtwo(2)
