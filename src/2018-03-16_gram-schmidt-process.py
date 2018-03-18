6cbeac7ecdfe7242d94f721e2b1832ad015d3d27
nayefahmad <nayef.ahmad.11@gmail.com>
2018-03-17 17:19:14 -0700

    edits to existing code

--- src/2018-03-16_gram-schmidt-process.py
+++ src/2018-03-16_gram-schmidt-process.py
@@ -14,22 +14,44 @@ verySmallNumber = 1e-14 # That's 1×10⁻¹⁴ = 0.00000000000001
 # Our first function will perform the Gram-Schmidt procedure for 4 basis vectors.
 # We'll take this list of vectors as the columns of a matrix, A.
 # We'll then go through the vectors one at a time and set them to be orthogonal
-# to all the vectors that came before it. Before normalising.
+#   to all the vectors that came before it. Before normalising.
 # Follow the instructions inside the function at each comment.
 # You will be told where to add code to complete the function.
+
+# function definition: ---------
 def gsBasis4(A) :
     B = np.array(A, dtype=np.float_) # Make B as a copy of A, since we're going to alter it's values.
     # The zeroth column is easy, since it has no other vectors to make it normal to.
     # All that needs to be done is to normalise it. I.e. divide by its modulus, or norm.
+    
+    print("original B[:,0]=", B[:,0])
+    # print(la.norm(B[:,0]))
     B[:, 0] = B[:, 0] / la.norm(B[:, 0])
+    print("final B[:,0]=",B[:,0])
+
+
     # For the first column, we need to subtract any overlap with our new zeroth vector.
-    B[:, 1] = B[:, 1] - B[:, 1] @ B[:, 0] * B[:, 0]
+    print("original B[:,1]=", B[:,1])
+    print("dot prod: B[:, 1] @ B[:, 0]=", B[:, 1] @ B[:, 0])
+    print("^^this doesn't seem right")  # todo: 
+    print("vector result: (B[:, 1] @ B[:, 0]) * B[:, 0]=", B[:, 1] @ B[:, 0] * B[:, 0])
+    
+    B[:, 1] = B[:, 1] - (B[:, 1] @ B[:, 0]) * B[:, 0]
+    print("final B[:, 1]=", B[:, 1])
+    # @ is an infix operator for matrix multiplication (dot product in this case)
+    # see https://www.python.org/dev/peps/pep-0465/ 
+    
+    # * does element-wise multiplication of a scalar with each element of a vector
+    
+    
     # If there's anything left after that subtraction, then B[:, 1] is linearly independant of B[:, 0]
     # If this is the case, we can normalise it. Otherwise we'll set that vector to zero.
-    if la.norm(B[:, 1]) > verySmallNumber :
+    if la.norm(B[:, 1]) > verySmallNumber :  # i.e. approx not zero
         B[:, 1] = B[:, 1] / la.norm(B[:, 1])
     else :
         B[:, 1] = np.zeros_like(B[:, 1])
+    
+    
     # Now we need to repeat the process for column 2.
     # Insert two lines of code, the first to subtract the overlap with the zeroth vector,
     # and the second to subtract the overlap with the first.
@@ -53,6 +75,12 @@ def gsBasis4(A) :
     
     # Finally, we return the result:
     return B
+
+
+
+#***********************************************************************************
+#  PART TWO 
+#***********************************************************************************
 
 # The second part of this exercise will generalise the procedure.
 # Previously, we could only have four vectors, and there was a lot of repeating in the code.
@@ -80,3 +108,38 @@ def gsBasis(A) :
 # the sum of all the norms will be the dimension.
 def dimensions(A) :
     return np.sum(la.norm(gsBasis(A), axis=0))
+
+
+
+
+#***********************************************************************************
+# TEST THE FUNCTION
+#***********************************************************************************
+
+V = np.array([[1,0,2,6],
+              [0,1,8,2],
+              [2,8,3,1],
+              [1,-6,2,3]], dtype=np.float_)
+gsBasis4(V)
+
+
+
+# Once you've done Gram-Schmidt once,
+# doing it again should give you the same result. Test this:
+U = gsBasis4(V)
+gsBasis4(U)
+
+
+# Try the general function too.
+gsBasis(V)
+
+
+# See what happens for non-square matrices
+A = np.array([[3,2,3],
+              [2,5,-1],
+              [2,4,8],
+              [12,2,1]], dtype=np.float_)
+gsBasis(A)
+
+dimensions(A)
+
