64fad92e125657479a57ea0d497c7b352767a430
nayefahmad <nayef.ahmad.11@gmail.com>
2018-04-01 19:04:00 -0700

    week 5 quiz

--- src/2018-03-25_matrix-multiplications.py
+++ src/2018-03-25_matrix-multiplications.py
@@ -42,4 +42,123 @@ print(T)
 
 
 
+#*****************************************************
+# random calculations: -------------------------------
+#*****************************************************
+
+# WEEK 5 QUIZ ***************************************
+# QUESTION 1: 
+
+# Eigenvalues
+M = np.array([[1, 0, 0],
+              [0, 2, 0],
+              [0, 0, 3]])
+vals, vecs = np.linalg.eig(M)
+vals
+vecs
+
+# the eigenvectors of M are the original basis vectors: they are simply 
+#   scaled by factors 1, 2, 3; no change in direction. 
+
+M = np.array([[4, -5, 6],
+              [7, -8, 6],
+              [3/2, -1/2, -2]])
+
+M @ np.array([-3, -3, -1]) == np.array([-3, -3, -1])  # is eigenvec
+M @ np.array([.5, -.5, -1]) == np.array([.5, -.5, -1])  # not eigenvec
+M @ np.array([-3,-2,1]) == np.array([-3,-2,1])  # not eigenvec
+M @ np.array([-1,1,-2]) == np.array([-1,1,-2]) # not eigenvec
+M @ np.array([-2/sqrt(9), -2/sqrt(9), 1/sqrt(9)])  == np.array([-2/sqrt(9), -2/sqrt(9), 1/sqrt(9)])  # not eigenvec
+M @ np.array([1/sqrt(6), -1/sqrt(6), 2/sqrt(6)]) ==  np.array([1/sqrt(6), -1/sqrt(6), 2/sqrt(6)])  # not eigenvec
+
+
+
+# QUESTION 2: 
+L = np.array([[0, 0, 0, 1 ],
+              [1, 0, 0, 0 ],
+              [0, 1, 0, 0 ],
+              [0, 0, 1, 0 ]])
+    
+L_vals, L_vecs = np.linalg.eig(L)
+L_vals  # some eigenvalues are complex 
+L_vecs  # this is a 4 x 4 matrix, but with complex entries. 
+
+
+
+# QUESTION 3: --------------------------
+L_prime = np.array([[0.1, 0.1, 0.1, 0.7],
+                    [0.7, 0.1, 0.1, 0.1],
+                    [0.1, 0.7, 0.1, 0.1],
+                    [0.1, 0.1, 0.7, 0.1]])
+        
+L_prime_vals, L_prime_vecs = np.linalg.eig(L_prime)
+L_prime_vals  # some eigenvalues are complex 
+L_prime_vecs  # this is a 4 x 4 matrix, but with complex entries. 
+
+
+
+# QUESTION 4: BLOCK DIAGONAL LINK MATRIX: ---------------------
+L_block = np.array([[0, 1, 0, 0],
+                    [1, 0, 0, 0],
+                    [0, 0, 0, 1],
+                    [0, 0, 1, 0]])
+    
+L_block_vals, L_block_vecs = np.linalg.eig(L_block)
+L_block_vals  # some eigenvalues are complex; none is equal to 1.0 
+L_block_vecs  # this is a 4 x 4 matrix, but with complex entries. 
+
+np.linalg.det(L_block)  # non-zero determinant 
+
+
+# let's add damping:
+L_block_damped = np.array([[0.1, 0.7, 0.1, 0.1],
+                           [0.7, 0.1, 0.1, 0.1],
+                           [0.1, 0.1, 0.1, 0.7],
+                           [0.1, 0.1, 0.7, 0.1]])
+
+L_block_damp_vals, L_block_damp_vecs = np.linalg.eig(L_block_damped)
+L_block_damp_vals  
+L_block_damp_vecs 
+
+
+
+# QUESTION 8: FIND EIGENVECTORS: -------------------------------
+
+M = np.array([[3/2, -1],
+              [-.5, .5]])
+M_vals, M_vecs = np.linalg.eig(M)
+M_vals
+M_vecs
+
+v1 = np.array([1-sqrt(5), 1])
+v2 = np.array([-1-sqrt(3), 1])
+v3 = np.array([-1-sqrt(5), 1])
+v4 = np.array([1-sqrt(3), 1])
+
+# check v1
+M @ v1 == (v1 * M_vals[0])
+M @ v1 == (v1 * M_vals[1])
+
+# check v2: 
+M @ v2 == (v2 * M_vals[0])
+M @ v2 == (v2 * M_vals[1])
+
+# check v3
+M @ v3 == (v3 * M_vals[0])
+M @ v3 == (v3 * M_vals[1])
+
+# check v4: 
+M @ v4 == (v4 * M_vals[0])
+M @ v4 == (v4 * M_vals[1])
+
+
+
+# QUESTION 10: FIND A-squared: ------------------
+
+
+
+
+    
+    
+
 
