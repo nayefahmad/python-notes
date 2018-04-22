790b8903a0e788fe385f00f525112cf924b56bc3
nayefahmad <nayef.ahmad.11@gmail.com>
2018-04-22 15:51:08 -0700

    more complicated network

--- src/2018-04-22_simple-neural-networks.py
+++ src/2018-04-22_simple-neural-networks.py
@@ -38,9 +38,35 @@ a1(1)
 #****************************************************************
 # More complicated network: ----------------
 #****************************************************************
+# First set up the network.
+sigma = np.tanh
+W = np.array([[-2, 4, -1],[6, 0, -3]])
+b = np.array([0.1, -2.5])
+
+# Define our input vector
+x = np.array([0.3, 0.4, 0.1])
+
+# define fn to get scalar outputs: 
+def calc_output(input_vec, 
+                weight_matrix, 
+                bias_vector, 
+                index): 
+    
+    # index starts at 0 to represent first output
+    
+    return(sigma((W[index] @ input_vec) + bias_vector[index]))
+
+# evaluate first output, a1_0: 
+a1_0 = calc_output(x, W, b, 0)
+
+# evaluate second output, a1_1
+a1_1 = calc_output(x, W, b, 1)    
 
 
+    
+a1 = np.array([a1_0, a1_1])
 
+a1
 
 
 
