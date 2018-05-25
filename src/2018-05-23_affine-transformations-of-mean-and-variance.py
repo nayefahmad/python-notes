3f5518c6e2854fbb500f08d7b4dbe61743b9691b
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-05-25 15:48:33 -0700

    time function + measure change in mean calculation time as size of dataset increases

--- src/2018-05-23_affine-transformations-of-mean-and-variance.py
+++ src/2018-05-23_affine-transformations-of-mean-and-variance.py
@@ -23,8 +23,10 @@ import timeit
 from ipywidgets import interact
 
 
-
+# --------------------------------------------------------------------------------
 # 1. Olivetti Faces dataset: ------------------------------
+# --------------------------------------------------------------------------------
+
 '''
 The dataset we have are usually stored as 2D matrices, then it would be really 
 important to know which dimension represents the dimension of the dataset, and 
@@ -99,8 +101,10 @@ def cov_naive(X):
     return covariance    
 
 
-
+# --------------------------------------------------------------------------------
 # 3. MEAN AND COVARIANCE USING VECTORIZED OPERATIONS: ------------------------
+# --------------------------------------------------------------------------------
+
 def mean(X):
     """Compute the mean for a dataset
     
@@ -154,14 +158,100 @@ plt.imshow(mean_face(faces).reshape((64, 64)), cmap='gray');
 
 
 
-
+# --------------------------------------------------------------------------------
 # 4. PERFORMANCE BENCHMARKING WITH TIME FUNCTIONS: -------------------------------------------
+# --------------------------------------------------------------------------------
+
 # We have some huge data matrix, and we want to compute its mean
 X = np.random.randn(100000, 20)
 # Benchmarking time for computing mean
 %time mean_naive(X)
 %time mean(X)
 pass
+
+
+'''
+Alternatively, we can also see how running time increases as we increase the 
+size of our dataset. In the following cell, we run mean, mean_naive and cov, 
+cov_naive for many times on different sizes of the dataset and collect their
+running time. If you are less familiar with Python, you may want to spend 
+some time understanding what the code does. 
+''''
+ 
+# define function to measure time: 
+def time(f, repeat=100):
+    """A helper function to time the execution of a function.
+    
+    Arguments
+    ---------
+    f: a function which we want to time it.
+    repeat: the number of times we want to execute `f`
+    
+    Returns
+    -------
+    the mean and standard deviation of the execution.
+    """
+    times = []  # initialize empty list to collect durations 
+    
+    for _ in range(repeat):  # todo: why use underscore here instead of character like i? 
+        start = timeit.default_timer()
+        f()  # call the function 
+        stop = timeit.default_timer()
+        times.append(stop-start)  # calculate duration 
+    
+    return np.mean(times), np.std(times)
+
+
+
+
+# now measure difference between naive and vectorized mean: 
+fast_time = []
+slow_time = []
+
+for size in np.arange(100, 300, step=100):  # todo: what is np.arrange()  ? 
+    X = np.random.randn(size, 20)
+    
+    f = lambda : mean(X)  # first pass the vectorized mean fn to time( )
+    mu, sigma = time(f)
+    fast_time.append((size, mu, sigma))
+    
+    f = lambda : mean_naive(X)  # then pass the naive mean to time( ) 
+    mu, sigma = time(f)
+    slow_time.append((size, mu, sigma))
+
+fast_time = np.array(fast_time)
+slow_time = np.array(slow_time)    
+
+fast_time
+slow_time
+
+
+# plot the change in time with change in size of input using matplotlib: 
+fig, ax = plt.subplots()
+
+ax.errorbar(fast_time[:,0], fast_time[:,1], fast_time[:,2], label='fast mean', linewidth=2)
+ax.errorbar(slow_time[:,0], slow_time[:,1], slow_time[:,2], label='naive mean', linewidth=2)
+
+ax.set_xlabel('size of dataset')
+ax.set_ylabel('running time')
+plt.legend(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
