c1b5967b1ac75e1bc40b2285822f11cea73570f0
nayefahmad <nayef.ahmad.11@gmail.com>
2018-05-27 16:16:52 -0700

    more examples

--- src/2018-05-27_inner-products-for-lengths-and-distances.py
+++ src/2018-05-27_inner-products-for-lengths-and-distances.py
@@ -16,14 +16,61 @@ import numpy as np
 # Q1 ---------------------------------------------------------------------
 x = np.array([1, -1, 3])
 
+M = np.array([[2, 1, 0],
+              [1, 2, -1], 
+              [0, -1, 2]])
+    
 
 # define inner prod fn: 
-def innerprod(x): 
-    M = np.array([[2, 1, 0],
-                  [1, 2, 1], 
-                  [0, -1, 2]])
-    
-    return np.transpose(x) @ M @ x 
+def innerprod(x, y, matrix): 
+    return np.transpose(x) @ matrix @ y
 
 # call the fn: 
-innerprod(x)
+innerprod(x, x, M)
+
+
+
+# Q2 ---------------------------------------------------------------------
+
+x = np.array([.5, -1, -.5])
+y = np.array([0, 1, 0])
+
+M = np.array([[2, 1, 0],
+              [1, 2, -1], 
+              [0, -1, 2]])
+
+innerprod(x-y, x-y, M)
+
+
+# Q3 ---------------------------------------------------------------------
+x = np.array([-1, 1])
+
+M = np.array([[5, -1], 
+              [-1, 5]])
+
+M = .5 * M
+
+innerprod(x, x, M)
+
+
+# Q4 ---------------------------------------------------------------------
+x = np.array([.5, -1, -.5])
+y = np.array([0, 1, 0])
+
+M = np.array([[2, 1, 0],
+              [1, 2, -1], 
+              [0, -1, 2]])
+
+innerprod(x-y, x-y, M)
+
+dist = np.sqrt(innerprod(x-y, x-y, M))
+dist
+
+
+# Q5 ---------------------------------------------------------------------
+x = np.array([-1, -1, -1])
+
+np.transpose(x) @ x
+
+
+
