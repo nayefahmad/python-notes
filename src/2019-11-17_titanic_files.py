b4d7051363ac4c8cf04febed9fc23518589eba9e
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-11-18 22:16:55 -0800

    Update 2019-11-17_titanic_files.py

--- src/2019-11-17_titanic_files.py
+++ src/2019-11-17_titanic_files.py
@@ -7,17 +7,24 @@ Created on Sun Nov 17 02:58:22 2019
 
 import numpy as np
 import pandas as pd
-import statsmodels as sm 
-import os 
+import statsmodels as sm
+import os
 
 os.getcwd()
 
 df1 = pd.read_csv("D:/pydata-book/datasets/titanic/train.csv")
 
 df1.shape
-df1.columns 
+len(df1)
+df1.columns
+df1.info()
+
 df1.head
 df1.tail
 
-df1.plot(kind = "scatter", x = "Age", y = "Fare")
+df1['Age']
+df1['Survived']
 
+
+
+p = df1.plot(kind = "scatter", x = "Age", y = "Fare")
