6e87756200acbfabc8713c8e32dd6c8a1a39fe15
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-11-19 12:00:05 -0800

    create pd.DataFrame from np arrays

--- src/2019-11-18_pandas-library-exploration.py
+++ src/2019-11-18_pandas-library-exploration.py
@@ -13,7 +13,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels as sm
 
-# %matplotlib 
+%matplotlib inline  # other option is: `%matplotlib qt5`
+
+
 
 #%% Create a pd dataframe: examples from docs 
 
@@ -24,16 +26,54 @@ df.dtypes
 
 
 
-#%% 
+#%% Create a pd dataframe from np arrays 
 
-# Create a pd dataframe 
-
-col1 = np.arange(1, 10)
+np.random.seed(10)
+col1 = np.arange(0, 10)
 col2 = np.random.rand(10)
 
-np.c_[[col1], [col2]]
+type(col1)
+type(col2)
+len(col1)
+#dir(col1)
 
-df1 = pd.DataFrame(data = np.c_[[col1], [col2]])
 
+# Option 1: 
+# call (pd.DataFrame) with a dictionary passed to the `data` arg 
+df1 = pd.DataFrame(data = {'col_1':col1, 'col_2':col2})
+
+# Option 2: 
+# 
+# np.concatenate((col1, col2), axis=1)  # doesn't work 
+np.stack((col1, col2), axis = 1)  # works, but forces all numbers to be floats? 
+
+df2 = pd.DataFrame(data = np.stack((col1, col2), axis = 1), 
+                   columns = ['col1', 'col2'])
+
+df1
+df2
+
+
+
+
+
+
+
+#%% Examinig the pd dataframe 
 df1.shape
+df1.describe
+df1.info
+df1.dtypes
+df1.columns  # colnames 
+df1.columns[1]
+df1[:-5]  # drop last 5 rows 
 
+
+df2.dtypes  # note that both cols were coerced to floats when we stacked the np arrays 
+
+
+#%%Plots 
+
+df1.plot.line()  # note that we need the parens at the end here 
+
+plt.plot(['col_1'], ['col_2'], 'ro--')
